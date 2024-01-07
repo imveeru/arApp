0e8063539c47d7b9ce0950caab4b1c6a5eabc894
imveeru <vind.veeru@gmail.com>
2024-01-08 02:17:58 +0530

    Login Page backend

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+let loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let mail = document.getElementById("email").value;
+    let password = document.getElementById("pwd").value;
+
+    const apiUrl = "https://gfsk-backend.onrender.com";
+
+    
+});
