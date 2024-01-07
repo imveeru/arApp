2a922b56523faebb99b3bebc38508ab34f5222a4
imveeru <vind.veeru@gmail.com>
2024-01-08 02:21:02 +0530

    Login Page backend

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,5 @@
 let loginForm = document.getElementById("loginForm");
+let errorDiv = document.getElementById("errDiv");
 
 loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -6,7 +7,27 @@ loginForm.addEventListener("submit", (e) => {
     let mail = document.getElementById("email").value;
     let password = document.getElementById("pwd").value;
 
-    const apiUrl = "https://gfsk-backend.onrender.com";
+    const jsonData={
+        "email":mail,
+        "password":password
+    }
 
-    
+    const apiUrl = "https://gfsk-backend.onrender.com/login";
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // Add any additional headers as needed
+        },
+        body: JSON.stringify(jsonData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
 });
