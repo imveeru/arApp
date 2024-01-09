cc0b90c3ba934463a7d171ace9adf5267f56819b
imveeru <vind.veeru@gmail.com>
2024-01-09 05:59:09 +0530

    Hint for GT

---
+++ scripts/hint_gt.js
@@ -0,0 +1,8 @@
+let one=document.getElementById("one")
+let two=document.getElementById("two")
+let nextBtn=document.getElementById("next-btn")
+
+function changePage(){
+    one.style.display="none";
+    two.style.display="block";
+}
