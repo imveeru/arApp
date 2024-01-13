25232c186de5da6deda781e89cb69937d893c43a
imveeru <vind.veeru@gmail.com>
2024-01-13 21:57:19 +0530

    Image map

--- scripts/home.js
+++ scripts/home.js
@@ -3,24 +3,33 @@ let playerScore=document.getElementById("playerScore");
 
 playerName.innerText=localStorage.getItem("userName");
 
-const apiUrl = "https://gfsk-backend.onrender.com/get-points/"+localStorage.getItem("userId");
+if(localStorage.getItem("userName")==undefined || localStorage.getItem("userName")=="Guest" || localStorage.getItem("userName")==null){
+    localStorage.setItem("userName","Guest")
+    playerName.innerText=localStorage.getItem("userName");
+}else{
 
-fetch(apiUrl, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        // Add any additional headers as needed
-    },
-    mode: 'cors'
-})
-.then(response => response.json())
-.then(data => {
-    console.log('Success:', data["points"]);
-    playerScore.innerText=data["points"];
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+    const apiUrl = "https://gfsk-backend.onrender.com/get-points/"+localStorage.getItem("userId");
+
+    fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            // Add any additional headers as needed
+        },
+        mode: 'cors'
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data["points"]);
+        playerScore.innerText=data["points"];
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+    console.log(localStorage.getItem("userName"));
+}
+
 
 let navOpen=document.getElementById("navOpen")
 let navClose=document.getElementById("navClose")
