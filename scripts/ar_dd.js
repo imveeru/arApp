bf1c9ef3c68518d4a04e45c8a57c109caedae973
imveeru <vind.veeru@gmail.com>
2024-01-13 15:55:07 +0530

    Success scan toast

--- scripts/ar_dd.js
+++ scripts/ar_dd.js
@@ -1,9 +1,13 @@
 let marker1=document.getElementById('animated-marker')
 let marker2=document.getElementById('animated-marker2')
 let marker3=document.getElementById('animated-marker3')
+let toast = document.getElementById("snackbar");
 
 let test=document.getElementById("animated-marker3")
 console.log(test.children[0].attr)
+
+let currModel=localStorage.getItem("modelId")
+console.log(currModel);
 
 let timerInterval;
 let totalSeconds = 0;
@@ -54,24 +58,35 @@ marker1.addEventListener("markerFound", (e)=>{
         "points":Math.ceil(100/minutes) 
     }
 
-    const apiUrl = "https://gfsk-backend.onrender.com/add-points";
+    if (currModel==9){
+        //alert("Watha");
+        toast.innerHTML="Wow, correct!"
+        toast.className = "show";
+        setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
+    
+        const apiUrl = "https://gfsk-backend.onrender.com/add-points";
 
-    fetch(apiUrl,{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        mode: 'cors',
-        body: JSON.stringify(jsonData),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        //window.location.href = "index.html";
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        fetch(apiUrl,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'cors',
+            body: JSON.stringify(jsonData),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            //window.location.href = "index.html";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }else{
+        
+    }
+
+
 })
 
 marker2.addEventListener("markerFound", (e)=>{ 
---
+++ scripts/hint_dd.js
@@ -0,0 +1,47 @@
+let clueTitle=document.getElementById("clueTitle")
+let clueText=document.getElementById("clueText")
+let nextBtn=document.getElementById("nextBtn")
+
+let model_id=Math.floor(Math.random() * 18);
+
+let clues;
+
+const cKeys=["c1","c2","c3","c4"]
+let cIndex=0;
+
+let apiUrl = `https://gfsk-backend.onrender.com/clue/DD/${model_id}`;
+
+fetch(apiUrl,{
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    mode: 'cors',
+})
+.then(response => response.json())
+.then(data => {
+    console.log('Success:', data);
+    console.log(data);
+    localStorage.setItem("gameId",data["gameId"])
+    localStorage.setItem("modelId",data["modelId"])
+    clues=data["clue"][0];
+
+    clueText.innerHTML=clues[cKeys[cIndex]]
+
+    //window.location.href = "index.html";
+})
+.catch(error => {
+    console.error('Error:', error);
+});
+
+function changeClue(){
+    if(cIndex<3){
+        cIndex++;
+    }
+    clueTitle.innerHTML="Clue #"+(cIndex+1);
+    clueText.innerHTML=clues[cKeys[cIndex]]
+    if(cIndex==3){
+        nextBtn.innerHTML="Start Game"
+    }
+    console.log(cIndex);
+}
