da2e00c797001d1f398468fe4c3587f6f1615941
imveeru <vind.veeru@gmail.com>
2024-01-29 20:15:11 +0530

    Updated hint

--- scripts/hint_dd.js
+++ scripts/hint_dd.js
@@ -2,7 +2,7 @@ let clueTitle=document.getElementById("clueTitle")
 let clueText=document.getElementById("clueText")
 let nextBtn=document.getElementById("nextBtn")
 
-let models=[1,3,6,7,8,9,10,11,16,18]
+let models=[1,3,6,8,9,10,11,12,14,18]
 let model_id=Math.floor(Math.random() * models.length);
 
 let clues;
