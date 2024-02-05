402d81c16927f5295c35756fc102bbf329577cf1
imveeru <vind.veeru@gmail.com>
2024-02-05 06:50:47 +0530

    Added badges

--- scripts/homewiz/ar_glu.js
+++ scripts/homewiz/ar_glu.js
@@ -2,11 +2,20 @@ let models=["m74","m78","m76","m77","m88","m89","m80","m81","m90","m87"]
 
 let model_names=["White Glue","Electrical tape","Super Glue","Magnets","Epoxy","Wood Glue","Double-sided tape","Putty","Velcro ","Duck tape"]
 
+let badge_name=["Paper Permanence","Electrical Explorer","Surface Bonding Sage","Metal Marvel","Preserving Maestro","Wooden Wonder","Twin Twined","Moldable Marvel","Fastening Maestro","Handy Fixer"]
+
 let elements=new Array(model_names.length);
 console.log(elements);
 
 const currModel=localStorage.getItem("hw_modelId");
 let toast=document.getElementById("snackbar");
+let isDonno=localStorage.getItem("hw_glu_clue")
+
+if(isDonno==1){
+    console.log("aamaa")
+    toast.innerHTML = `Find <br/> <img width="150" src="markers/${models[currModel]}.png" alt=${model_names[currModel]}/> <br/> ${model_names[currModel]}`;
+    toast.className = "show";
+}
 
 let successAudio=new Audio('audio/success.mp3')
 let failureAudio=new Audio('audio/failure.mp3')
@@ -56,22 +65,20 @@ for(i in models){
 
     elements[i].addEventListener("markerFound", (e)=>{ 
 
-        alert(models[i])
+        // alert(models[i])
 
         if (currModel==i){
-        
-        
-        const jsonData={
-            "userId":localStorage.getItem("userId"),
-            "gameId":"DD",
-            "points":Math.ceil(100/minutes) 
-        }
-    
             successAudio.play();
-            toast.innerHTML=model_names[i];
+            toast.innerHTML=`<img width="100" src="hw_badges/${badge_name[currModel]}.svg" alt=${badge_name[currModel]}/> <br/> ${model_names[i]}`;
             toast.className = "show";
             setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
     
+            const jsonData={
+                "userId":localStorage.getItem("userId"),
+                "gameId":"DD",
+                "points":Math.ceil(100/minutes) 
+            }
+
             const apiUrl = "https://gfsk-backend.onrender.com/add-points";
     
             fetch(apiUrl,{
--- scripts/homewiz/ar_tex.js
+++ scripts/homewiz/ar_tex.js
@@ -2,11 +2,20 @@ let models=["m64","m65","m66","m67","m68","m69","m70","m71","m72","m73"]
 
 let model_names=["Metal tumbler","wooden ladle","sandpaper","Polished glass","comb","Aluminum foil","Ceramic","Plastic","Toy Drum","Sponge"]
 
+let badge_name=["Thermal Conductor","Thermal resistor ","Friction Forager","Sleek Surface Seeker","Abrasive Ally","Electrical Explorer","Delicate Guardian","Sturdy Sentinel","Vibration Voyager","Sound Sculptor"]
+
 let elements=new Array(model_names.length);
 console.log(elements);
 
 const currModel=localStorage.getItem("hw_modelId");
 let toast=document.getElementById("snackbar");
+let isDonno=localStorage.getItem("hw_tex_clue")
+
+if(isDonno==1){
+    console.log("aamaa")
+    toast.innerHTML = `Find <br/> <img width="150" src="markers/${models[currModel]}.png" alt=${model_names[currModel]}/> <br/> ${model_names[currModel]}`;
+    toast.className = "show";
+}
 
 let successAudio=new Audio('audio/success.mp3')
 let failureAudio=new Audio('audio/failure.mp3')
@@ -56,21 +65,18 @@ for(i in models){
 
     elements[i].addEventListener("markerFound", (e)=>{ 
 
-        alert(models[i])
-
         if (currModel==i){
-        
-        
-        const jsonData={
-            "userId":localStorage.getItem("userId"),
-            "gameId":"DD",
-            "points":Math.ceil(100/minutes) 
-        }
     
             successAudio.play();
-            toast.innerHTML=model_names[i];
+            toast.innerHTML=`<img width="100" src="hw_badges/${badge_name[currModel]}.svg" alt=${badge_name[currModel]}/> <br/> ${model_names[i]}`;
             toast.className = "show";
             setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
+
+            const jsonData={
+                "userId":localStorage.getItem("userId"),
+                "gameId":"DD",
+                "points":Math.ceil(100/minutes) 
+            }
     
             const apiUrl = "https://gfsk-backend.onrender.com/add-points";
     
